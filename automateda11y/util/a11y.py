7d1dc0af352285343ff458adf4b85ac5013e3f56
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2023-01-24 22:37:16 +0000

    Report Dir

--- automateda11y/util/a11y.py
+++ automateda11y/util/a11y.py
@@ -5,9 +5,15 @@ import uuid
 
 from pathlib import Path
 
+from automateda11y.settings import Settings
+
 
 def root_dir():
     return Path(__file__).parent.parent.__str__()
+
+
+def json_reports_dir():
+    return Settings.report_dir
 
 
 class A11y:
@@ -16,12 +22,14 @@ class A11y:
         self.page = page
 
     def execute(self, engine, params=None):
+        if json_reports_dir() is None:
+            raise Exception("Reports path is not set, 'Settings.report_dir = <report_path>' is mandate")
         self.page.wait_for_load_state("domcontentloaded")
         js_text = Path(root_dir() + "/resources/js/", engine.name.lower() + ".js").read_text()
         self.page.evaluate("async (js)=> await window.eval(js)", js_text)
         data = self.page.evaluate("async (param) => await axeData(param)", params.to_json()) if engine.value == 2 else \
             self.page.evaluate("async (param) => await getData(param)", params.to_json())
-        file_name = './reports/' + engine.name.lower() + '/json/' + uuid.uuid4().__str__() + '.json'
+        file_name = json_reports_dir() + '/' + engine.name.lower() + '/json/' + uuid.uuid4().__str__() + '.json'
         os.makedirs(os.path.dirname(file_name), exist_ok=True)
         with open(file_name, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
